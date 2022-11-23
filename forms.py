5801683acee9dd75a7e225139db168aaf4525c99
gvytautas <gvytautas90@gmail.com>
2022-11-23 20:44:04 +0200

    lesson 17; part 2

--- forms.py
+++ forms.py
@@ -1,7 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField, IntegerField
 from wtforms.validators import DataRequired
 from wtforms_sqlalchemy.fields import QuerySelectField
+import main
 
 
 class AddClientForm(FlaskForm):
@@ -11,3 +12,10 @@ class AddClientForm(FlaskForm):
 class CreateProductForm(FlaskForm):
     code = StringField('code', validators=[DataRequired()])
     name = StringField('name', validators=[DataRequired()])
+
+
+class CreateStockForm(FlaskForm):
+    quantity = IntegerField('quantity', validators=[DataRequired()])
+    product_id = QuerySelectField(
+        query_factory=main.Product.query.all, allow_blank=True, get_label="name", get_pk=lambda obj: str(obj)
+    )
--- main.py
+++ main.py
@@ -7,7 +7,6 @@
 """
 
 from flask import Flask, render_template, request, redirect, url_for
-from forms import AddClientForm, CreateProductForm
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
@@ -15,7 +14,8 @@ db = SQLAlchemy()
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "?``§=)()%``ÄLÖkhKLWDO=?)(_:;LKADHJATZQERZRuzeru3rkjsdfLJFÖSJ"
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///my_database'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///my_database?check_same_thread=False'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
 migrate = Migrate(app, db)
@@ -45,6 +45,8 @@ class Stock(db.Model):
 
 
 with app.app_context():
+    import forms
+
     db.create_all()
 
 
@@ -55,7 +57,7 @@ def index():
 
 @app.route('/add_client', methods=['GET', 'POST'])
 def add_client():
-    form = AddClientForm()
+    form = forms.AddClientForm()
     if request.method == 'POST':
         name = form.name.data
         client = Client(name=name)
@@ -74,7 +76,7 @@ def show_clients():
 
 @app.route('/add_product', methods=['GET', 'POST'])
 def add_product():
-    form = CreateProductForm()
+    form = forms.CreateProductForm()
     if request.method == 'POST':
         product = Product(name=form.name.data, code=form.code.data)
         db.session.add(product)
@@ -86,12 +88,20 @@ def add_product():
 
 @app.route('/add_stock', methods=['GET', 'POST'])
 def add_stock():
-    return render_template('add_stock.html')
+    form = forms.CreateStockForm()
+    if request.method == 'POST':
+        stock = Stock(quantity=form.quantity.data, product_id=form.product_id.data.id)
+        db.session.add(stock)
+        db.session.commit()
+        return redirect(url_for('index'))
+
+    return render_template('add_stock.html', form=form)
 
 
 @app.route('/show_stock')
 def show_stock():
-    return render_template('show_stock.html')
+    stock = Stock.query.all()
+    return render_template('show_stock.html', data=stock)
 
 
 if __name__ == '__main__':
